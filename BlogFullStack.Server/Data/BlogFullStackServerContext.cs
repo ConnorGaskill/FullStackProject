042e497c7b1929e3671248f00ff40d9c75de6109
Connor Gaskill <gaskillconnor@gmail.com>
2025-01-31 00:13:14 -0800

    finished models and controllers

--- BlogFullStack.Server/Data/BlogFullStackServerContext.cs
+++ BlogFullStack.Server/Data/BlogFullStackServerContext.cs
@@ -18,5 +18,57 @@ namespace BlogFullStack.Server.Data
         public DbSet<BlogFullStack.Server.Models.DynamicContent> DynamicContent { get; set; } = default!;
         public DbSet<BlogFullStack.Server.Models.FeedbackData> FeedbackData { get; set; } = default!;
         public DbSet<BlogFullStack.Server.Models.StaticContent> StaticContent { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BusinessContactDetails>().HasData(
+
+                new BusinessContactDetails
+                {
+                    BusinessContactID = 1,
+
+                    BusinessContactPhoneNumber = "(360) 123-4567",
+
+                    BusinessContactEmail = "VeryRealCo@RealMail.io",
+
+                    BusinessName = "ToastCo",
+
+                    BusinessAddress = "308 Negra Arroyo Lane, Albuquerque, New Mexico"
+
+                });
+
+            modelBuilder.Entity<DynamicContent>().HasData(
+
+            new DynamicContent
+            {
+                Name = "Test",
+
+                Body = "This is a post",
+
+                Author = "Arial Hughman",
+
+                CreatedDate = DateTime.Now,
+
+                LastModifiedDate = DateTime.Now,
+
+                Content = "Really cool stuff"
+
+            });
+
+            modelBuilder.Entity<FeedbackData>().HasData(
+                new FeedbackData
+                {
+                    FeedBackID = 1,
+
+                    Feedback = "I like toast"
+
+
+
+                }
+
+                );
+        }
     }
 }
